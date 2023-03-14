c0c170ef87b3498c84bf6ae0a63184e5b1270db9
DuarteFranca <duartefranca_@outlook.com>
2023-03-14 14:46:30 +0000

    home page image change

--- src/components/HomeCompo.jsx
+++ src/components/HomeCompo.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Card from 'react-bootstrap/Card';
 import '../homeCompon.css'
 import { Link } from 'react-router-dom';
+import Reviews from '../images/reviews.png';
+import Shop from '../images/shop.png';
+import MyProducts from '../images/myproducts.png';
+import MyCard from '../images/purchases.png';
 
 function HomeCompo() {
   return (
@@ -9,7 +13,7 @@ function HomeCompo() {
     <div>
     <Link to="/products">
         <Card className="bg-white text-black">
-      <Card.Img src="https://productmanagementfestival.com/wp-content/uploads/2017/01/sell-your-product-online.jpg" alt="Card image" />
+      <Card.Img src={Shop} alt="Card image" />
       <Card.ImgOverlay className="imgHover">
         <Card.Title className="title">Shop</Card.Title>
         
@@ -20,7 +24,7 @@ function HomeCompo() {
     <div>
     <Link to="/myProducts">
     <Card className="bg-white text-black">
-      <Card.Img  src="https://media.istockphoto.com/id/1388175035/photo/hand-truck-with-cardboard-boxes-on-blue-background-moving-house-concept-relocation-cargo.jpg?b=1&s=170667a&w=0&k=20&c=sBuqMuHKDtuF749Azh-Zo6XN1K7mt2ssitGoCoxJu10=" alt="Card image" />
+      <Card.Img  src={MyProducts} alt="Card image" />
       <Card.ImgOverlay className="imgHover">
         <Card.Title className="title">My products</Card.Title>
         
@@ -31,7 +35,7 @@ function HomeCompo() {
     <div>
     <Link to="/shoppingCart">
     <Card className="bg-white text-black">
-      <Card.Img src="https://gamipress.com/wp-content/uploads/2018/12/purchases.svg" alt="Card image" />
+      <Card.Img src={MyCard} alt="Card image" />
       <Card.ImgOverlay className="imgHover">
         <Card.Title className="title">Shopping Cart</Card.Title>
         
@@ -41,8 +45,8 @@ function HomeCompo() {
     </div>
     <div>
     <Link to="/reviews">
-    <Card className="bg-white text-white">
-      <Card.Img src="https://easydigitaldownloads.com/wp-content/uploads/edd/2016/04/product-reviews-featured-image.png" alt="Card image" />
+    <Card className="bg-white text-black">
+      <Card.Img src={Reviews} alt="Card image" />
       <Card.ImgOverlay className="imgHover">
         <Card.Title className="title">Reviews</Card.Title>
         
